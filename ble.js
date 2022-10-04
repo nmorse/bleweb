c9c36d08009a42ec73a6869547f28af0bf55d877
nmorse <morse.nate@gmail.com>
2022-10-03 20:51:18 -0400

    debug some parsing

--- ble.js
+++ ble.js
@@ -81,6 +81,7 @@ async function send_text(data) {
     data = data.slice(18);
     if (uartTxCharacteristic) {
       special_continue_char = data.length ? ',' : '' 
+      console.log("chunk+special_continue_char", chunk+special_continue_char)
       var sent = await uartTxCharacteristic.writeValueWithResponse(new TextEncoder().encode(chunk+special_continue_char));
       console.log(sent);
     }
